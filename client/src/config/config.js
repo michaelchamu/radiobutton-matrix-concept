c4e93b09c7a35ece9678e9907a196483d2a48005
Michael Bosomefi Chamunorwa <chamunorwamichael@gmail.com>
2018-11-03 04:37:37 +0200

    update API ref

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 // Configuration for Question service
 
-const API_URL = "http://localhost:3500/matrix";
+const API_URL = "http://localhost:3500/api/matrix";
 const HEIGHT = "5px";
 const WIDTH = "5px";
 // w92, w154, w185, w342, w500, w780, original
