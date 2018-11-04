d59641415752bbf2b9831193cfe7e7ee4a67a237
Michael Bosomefi Chamunorwa <chamunorwamichael@gmail.com>
2018-11-04 13:44:02 +0200

    delete routes separate file added

---
+++ server/routes/delete.route.js
@@ -0,0 +1,25 @@
+const _ = require('lodash');
+const fs = require("fs");
+const path = require('path');
+let { Matrix } = require('../Models/Matrix.model');
+module.exports = [
+	{
+  method: "DELETE",
+  path: "/api/matrix/{uniqueid}",
+  handler: (request, h) => {
+  	let uniqueid = request.params.uniqueid;
+  	let promise = new Promise((resolve, reject) => {
+  		Matrix.findOneAndDelete({'uniqueid': uniqueid}).then((record) => {
+  			if(!record){
+  				resolve({statusCode: 404});
+  			} else {
+  				resolve({statusCode:201});
+  			}
+  			}).catch((err) => {
+  			reject({statusCode: 400, message: err});
+  		})
+  	})
+  	return promise;
+  }
+},
+];
