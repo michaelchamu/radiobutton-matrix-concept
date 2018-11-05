05a335706601d36eecf0f63dd3a3469a4ffd8865
Michael Bosomefi Chamunorwa <chamunorwamichael@gmail.com>
2018-11-05 06:19:03 +0200

    (node:40225) DeprecationWarning: current URL string parser fix

--- server/db/db.config.js
+++ server/db/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://127.0.0.1:27017/matrix');
+mongoose.connect('mongodb://127.0.0.1:27017/matrix', { useNewUrlParser: true });
 let db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'connection error:'));
 	db.once('open', function() {
